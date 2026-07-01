75db4bc32b5400a86dbe64338e327df1069732e1
YEHIA_REX <yhyyw4949@gmail.com>
2026-07-01 22:50:47 +0300

    Delete database directory

--- database/__init__.py
+++
@@ -1,4 +0,0 @@
-from .connection import db_manager, get_db
-from .models import Base, User, Wallet, Transaction, InternalTransfer, AdminLog, RateLimit
-
-__all__ = ['db_manager', 'get_db', 'Base', 'User', 'Wallet', 'Transaction', 'InternalTransfer', 'AdminLog', 'RateLimit']
